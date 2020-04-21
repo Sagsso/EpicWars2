912c42c1add6c46a688688514af2099f128b7c2a
sagsso <santiagogalvez17@gmail.com>
2020-04-20 19:02:20 -0500

    Refixed merge

--- views/Index/index.php
+++ views/Index/index.php
@@ -3,12 +3,7 @@
     <form class="form-signin">
     <span class="mb-3 h3">The best <br> ROL GAME</span>
   <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-<<<<<<< HEAD
-        
-  <h1 class="h3 mb-3 font-weight-normal">The best Rol game eva </br> Please sign in</h1>
-=======
   <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
->>>>>>> valveg
   <label for="inputEmail" class="sr-only">Email address</label>
   <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
   <label for="inputPassword" class="sr-only">Password</label>
