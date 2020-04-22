16f6394480649531e7d927d7f6a64d0ad5e2a1b6
sagsso <santiagogalvez17@gmail.com>
2020-04-21 19:47:28 -0500

    Signup fixed

--- views/Index/signup.php
+++ views/Index/signup.php
@@ -1,5 +1,4 @@
 <?php $this->loadFragment("head"); ?>
-<<<<<<< HEAD
 <body class="signin-background">
   <div class="text-center card" style="width: 25%;">
     <form class="form-signin">
@@ -12,21 +11,6 @@
       <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
     </form>
   </div>
-=======
-
-<body class="text-center">
-  <form action="./../../database/login.php" class="form-signin" method="POST">
-    <span class="mb-3 h3">The best <br> ROL GAME</span>
-    <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Please sign up</h1>
-    <label for="inputEmail" class="sr-only">Email address</label>
-    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" name="username" required autofocus>
-    <label for="inputPassword" class="sr-only">Password</label>
-    <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password"  required>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
-    <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
-  </form>
->>>>>>> sangal
 </body>
 
 </html>
