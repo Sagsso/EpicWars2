70ea73b8021540678491f45dc2714b86c3ccf839
sagsso <santiagogalvez17@gmail.com>
2020-04-22 17:56:01 -0500

    characters_bl print problem fixed

--- businessLogic/characters_bl.php
+++ businessLogic/characters_bl.php
@@ -13,26 +13,10 @@ class Characters_bl
         $username = $_SESSION['username'];
         // $characterSelected = $_SESSION['characterselected'];
         $level = Connection::getInstance()->select('level', 'user_character', "username = '" . $username . "'")[0]["level"];
-        print_r($level);
         $limitLevel = $level + 2;
         //Obtener los personajes de otros usuarios entre los niveles cercanos.
         $result = Connection::getInstance()->select('*', 'user_character', "username != '$username' AND level BETWEEN $level AND $limitLevel");
         return $result;
     }
 
-    // public static function getAll()
-    // {
-    //     $patients = Patient::getAll();
-    //     return $patients;
-    // }
-
-    // public static function create($data)
-    // {
-    //     if (!isset($data["id"])) {
-    //         $data["id"] = null;
-    //     }
-    //     $patient = Patient::instanciate($data);
-    //     $result = $patient->create();
-    //     return $result;
-    // }
 }
